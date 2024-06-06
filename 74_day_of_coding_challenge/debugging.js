21c76484e6c722825629e23dcab5f10b0261f116
Hooriya M. Fareed <162635352+hooriyaa@users.noreply.github.com>
2024-06-06 17:34:43 +0500

    Add files via upload

---
+++ 74_day_of_coding_challenge/debugging.js
@@ -0,0 +1,13 @@
+"use strict";
+// * DEBUGGING *
+let age = 22;
+if (age > 20) {
+    console.log("pass");
+}
+else if (age < 20) {
+    console.log("return");
+}
+else {
+    console.log("reboot");
+}
+//# sourceMappingURL=debugging.js.map
